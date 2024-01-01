e4d7de15b12a4d85368039f30299eee89fc4c095
mohammed binyamin <binyam0068@gmail.com>
2024-01-01 15:03:19 +0530

    created items in the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './Navbar.css'
 import  logo from '../assets/logo.png'
+import cart_icon from '../assets/cart_icon.png'
 function Navbar() {
   return (
     <div className="navbar">
         <div className="nav-logo">
-            <img src={logo} alt=""  />
+            <img src={logo} alt="logo"  />
             <p>INSTYLE</p>
+        </div>
+        <ul className="nav-menu">
+            <li>Shop</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+        </ul>
+        <div className="nav-login-cart">
+            <button>Login</button>
+            <img src={cart_icon} alt="" />
         </div>
     </div>
   )
