6705176aa3d41c891af4f8aa3bf0b69d226bcde2
mohammed binyamin <binyam0068@gmail.com>
2024-01-01 15:41:54 +0530

    added everything to the navbar and gave the style to it

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import  logo from '../assets/logo.png'
 import cart_icon from '../assets/cart_icon.png'
 function Navbar() {
+
+    const [menu,setMenu]=useState("")
   return (
     <div className="navbar">
         <div className="nav-logo">
@@ -10,14 +12,16 @@ function Navbar() {
             <p>INSTYLE</p>
         </div>
         <ul className="nav-menu">
-            <li>Shop</li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
+            <li onClick={()=>setMenu("shop")}>Shop {menu === "shop" &&<hr/> }</li>
+            <li onClick={()=>setMenu("Mens")}>Mens {menu === "Mens" &&<hr/> }</li>
+            <li onClick={()=>setMenu("Womens")}>Womens {menu === "Womens" &&<hr/> }</li>
+            <li onClick={()=>setMenu("kids")}>Kids {menu === "kids" &&<hr/> }</li>
         </ul>
         <div className="nav-login-cart">
             <button>Login</button>
             <img src={cart_icon} alt="" />
+        <div className='nav-cart-count'>0</div>
+
         </div>
     </div>
   )
